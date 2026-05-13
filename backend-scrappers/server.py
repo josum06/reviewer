66386a3ed46b73782660794ff8622019e02d3159
Sumit Joshi <sumit.joshi0616@gmail.com>
2026-05-14 00:12:03 +0530

    fix render port

--- backend-scrappers/server.py
+++ backend-scrappers/server.py
@@ -203,4 +203,5 @@ def refresh_platform(platform):
     return jsonify(new_results)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
